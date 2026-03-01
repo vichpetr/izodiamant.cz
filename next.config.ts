595a6020c05049442a6dcf6cf97ce024a9ef3508
Petr Vích <petr@petrvich.eu>
2026-03-01 21:19:33 +0100

    fix: configure remote image patterns in next.config.ts to allow external images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i0.wp.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'izodiamant.cz',
+      },
+      {
+        protocol: 'http',
+        hostname: 'izodiamant.cz',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
