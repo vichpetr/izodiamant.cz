3dd34d7e94c24db50a016f8ea2f80db90e4075a3
Petr Vích <petr@petrvich.eu>
2026-03-02 15:36:01 +0100

    fix: set edge runtime for dynamic routes to fix cloudflare pages build

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,6 +1,8 @@
 import { Resend } from 'resend';
 import { NextResponse } from 'next/server';
 
+export const runtime = 'edge';
+
 export async function POST(request: Request) {
   const apiKey = process.env.RESEND_API_KEY;
 
